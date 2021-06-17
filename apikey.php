21852875f75e9efd2246f4a6f4666c5691de24d2
Rizki-Setiawan <17.rizkisetiawan@gmail.com>
2021-06-17 13:30:01 +0700

    Update APIKEY

--- apikey.php
+++ apikey.php
@@ -1,36 +1,38 @@
 <?php
-	
-	function getKey() {
-		return ["1234", "rahasia", "secret"];
-	}
 
-	function isValid($input) {
-		$apikey = $input["api_key"];
-		if (in_array($apikey, getKey())){
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
+function getKey() {
+    return ["1234", "rahasia", "xyz"];
+}
 
-	function jsonOut($status, $message, $data){
-		$respon = ["status"=>$status,"message"=>$message,"data"=>$data];
-		header("Content-type: application/json");
-		echo json_encode($respon);
-	}
+function isValid($input) {
+    $apikey = $input["api_key"];
+    if (in_array($apikey, getKey())) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-	function getFilm(){
-		$film = [
-			["title"=>"FP5"],
-			["title"=>"FP2"]
-		];
-	}
+function jsonOut($status, $message, $data) {
+    $respon = ["status" => $status, "message" => $message, "data" => $data];
 
-	if (isValid($_GET)){
-		jsonOut("Berhasil","apikey valid",getFilm());
-	} else {
-		jsonOut("Gagal","apikey not valid",null);
-	}
+    header("Content-type: application/json");
+    echo json_encode($respon);
+}
+
+function getFilm() {
+    $film = [
+        ["title" => "FF9", "konten" => "film ini film ke-9"],
+        ["title" => "FF1", "konten" => "film ini film ke-1"],
+        ["title" => "FF2", "konten" => "film ini film ke-2"]
+    ];
+    return $film;
+}
+
+if (isValid($_GET)) {
+    jsonOut("berhasil", "apikey valid", getFilm());
+} else {
+    jsonOut("gagal", "apikey not valid!!!", null);
+}
 
 ?>
